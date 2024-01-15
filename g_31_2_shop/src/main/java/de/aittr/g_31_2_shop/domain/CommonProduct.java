907e64352803b3bd126f4cb8645db2f0e0caca99
aliyasagut <aliya.sagut@gmail.com>
2024-01-15 23:36:31 +0100

    add project

---
+++ g_31_2_shop/src/main/java/de/aittr/g_31_2_shop/domain/CommonProduct.java
@@ -0,0 +1,60 @@
+package de.aittr.g_31_2_shop.domain;
+
+import java.util.Objects;
+
+public class CommonProduct implements Product {
+
+    private int id;
+    private boolean isActive;
+    private String name;
+    private double price;
+
+    public CommonProduct() {
+        isActive = true;
+    }
+
+    public CommonProduct(int id, boolean isActive, String name, double price) {
+        this.id = id;
+        this.isActive = isActive;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isActive() {
+        return isActive;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CommonProduct that = (CommonProduct) o;
+        return id == that.id && isActive == that.isActive && Double.compare(that.price, price) == 0 && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, isActive, name, price);
+    }
+
+    @Override
+    public String toString() {
+        return  String.format("ID - %d, Name - %s, price - %f.2, isActive - %s.", id, name, price, isActive ? "yes" : "no");
+    }
+}
---
+++ g_31_2_shop/src/main/java/de/aittr/g_31_2_shop/domain/Product.java
@@ -0,0 +1,9 @@
+package de.aittr.g_31_2_shop.domain;
+
+public interface Product {
+
+    int getId();
+    boolean isActive();
+    String getName();
+    double getPrice();
+}
