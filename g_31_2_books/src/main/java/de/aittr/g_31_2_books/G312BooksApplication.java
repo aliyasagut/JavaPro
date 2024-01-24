41780a5acab5c7693681139836aa2081e823c615
aliyasagut <aliya.sagut@gmail.com>
2024-01-24 10:29:35 +0100

    create project

---
+++ g_31_2_books/src/main/java/de/aittr/g_31_2_books/G312BooksApplication.java
@@ -0,0 +1,13 @@
+package de.aittr.g_31_2_books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G312BooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G312BooksApplication.class, args);
+	}
+
+}
---
+++ g_31_2_shop/src/main/java/de/aittr/g_31_2_shop/services/CommonCustomerService.java
@@ -0,0 +1,2 @@
+package de.aittr.g_31_2_shop.services;public class CommonCustomerService {
+}
